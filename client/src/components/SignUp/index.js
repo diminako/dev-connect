119ffc46d9a532811deb8cc08d63bdacbd509e43
David Dugle <david.dugleii@yahoo.com>
2021-01-20 16:39:27 -0500

    signup page set up for now

--- client/src/components/SignUp/index.js
+++ client/src/components/SignUp/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 
-function SignUp() {
+const SignUp = () => {
+  const [registerUsername, setRegisterUsername] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+
+  const register = () =>{
+    axios({
+      method: "POST",
+      data: {
+        username: registerUsername,
+        password: registerPassword,
+      },
+      withCredentials: true,
+      url: "http://localhost:3000/register",
+    }).then((res) => console.log(res));
+  };
+
+
+
+
+
   return (
     <div>
     <div className="container">
@@ -11,6 +31,30 @@ function SignUp() {
                     <h1 className="text-center">Sign Up Page</h1>
                    
                 </div>
+                <div className="callout">
+            <form>
+              <div className="grid-container">
+                <div className="grid-x grid-padding-x">
+                      <div className="small-8 cell">
+                        <label>
+                          Username
+                          <input type="text" onChange={e => setRegisterUsername(e.target.value)} placeholder="username" />
+                        </label>
+                      </div>
+                      <div className="small-8 cell">
+                        <label>
+                          Password
+                          <input type="password" onChange={e => setRegisterPassword(e.target.value)} placeholder="password" />
+                        </label>
+                      </div>
+                      <div className="small-8 cell">
+                        <button onClick={register} >Submit</button>
+                      </div>
+                    </div>
+                  </div>
+            </form>
+          </div>
+
 
             </div>
 
