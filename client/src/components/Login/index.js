003adff66f58c0a4bb3348c5212f70cc44dbf852
Avery Brown-Gathings <72925420+averyjbrown2@users.noreply.github.com>
2021-01-20 14:02:26 -0500

    editing navbar

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -3,42 +3,31 @@ import React from "react";
 function Login() {
   return (
     <>
-      <div className="row">
-        <div className="columns small-12 small-centered">
-          <h1>Login</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="columns small-12">
-          <div className="callout"></div>
-          <form>
-            <div className="grid-container">
-              <div className="grid-x grid-padding-x">
-                <div className="row">
-                  <div className="columns">
-                    <div className="small-12 cell">
-                      <label>
-                        Username
-                        <input type="text" placeholder="username" />
-                      </label>
+  
+
+          <h1 class="align-right">Login</h1>
+      
+          <div className="callout">
+            <form>
+              <div className="grid-container">
+                <div className="grid-x grid-padding-x">
+                      <div className="small-8 cell">
+                        <label>
+                          Username
+                          <input type="text" placeholder="username" />
+                        </label>
+                      </div>
+                      <div className="small-8 cell">
+                        <label>
+                          Password
+                          <input type="text" placeholder="password" />
+                        </label>
+                      </div>
                     </div>
                   </div>
-                </div>
-                <div className="row">
-                  <div className="columns">
-                    <div className="small-12 cell">
-                      <label>
-                        Password
-                        <input type="text" placeholder="password" />
-                      </label>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
+            </form>
+          </div>
+        
     </>
   );
 }
