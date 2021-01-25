fdd98bb206748765c6b8ab4c9783db2b061c61d1
David Dugle <david.dugleii@yahoo.com>
2021-01-25 10:00:04 -0500

    have the logout working

---
+++ client/src/components/Logout/index.js
@@ -0,0 +1,29 @@
+// import React, { useEffect } from "react";
+// import axios from "axios";
+
+// const Logout = () => {
+
+
+//     useEffect(() =>{
+        
+//         console.log("mounting is hitting");
+        
+//         axios.get("/api/logout")
+
+//         // axios({
+//         //     method: "GET",
+//         //     withCredentials: true,
+//         //     url: "/api/logout"
+//         // })
+        
+//     })
+    
+    
+//     return(
+//         <h1>Logging Out!</h1>
+//     )
+
+    
+// }
+
+// export default Logout;
