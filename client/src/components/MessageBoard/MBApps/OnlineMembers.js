94feffb8e99b8f9887389f74f7cb80cdc6fade6b
Lorena Ramirez <ramirezlorenaa18@gmail.com>
2021-01-26 18:06:45 -0500

    mapped through onlineUsers prop and displayed as li

--- client/src/components/MessageBoard/MBApps/OnlineMembers.js
+++ client/src/components/MessageBoard/MBApps/OnlineMembers.js
@@ -1,28 +1,25 @@
 import React from "react";
 
 
-function OnlineMembers() {
+function OnlineMembers({ onlineUsers }) {
     return (
         <>
             {/* <h5 className="text-center caveat"></h5>
             <div className="grid-container chat">
                 <div className="grid-x grid-padding-x"> */}
-                        <div className="card online-members">
-                        <h4 className="card-divider online caveat">Online Members</h4>
-                        {/* <div className="small-1 cell"></div>
-                        <div className="small-3 cell">*/}
-                            <ul className=""> 
-                                <li className="header ">Dimi</li>
-                                <li className="header">David</li>
-                                <li className="header">Avery</li>
-                                <li className="header">Lorena</li>
-                                <li className="header">Randy</li>
-                               
-                               </ul>
-                         {/* <div className="small-8 cell"></div> */}
-                    {/* </div> */}
-                {/* </div>*/}
-            </div> 
+            <div className="card online-members">
+                <h4 className="card-divider online caveat">Online Members</h4>
+                <ul className="">
+
+                    {onlineUsers.map(user =>
+                    (
+                        <li className="header"> {user} </li>
+                    )
+                    )
+                    }
+
+                </ul>
+            </div>
         </>
 
     );
